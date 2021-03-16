223ba552fff871c94e248a4b0fa8120fafff8f7c
A7abhilash <a7abhilash@gmail.com>
2021-03-16 21:36:58 +0530

    added adding new expense function

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Alert, Button, StyleSheet, View } from "react-native";
 import AddBudgetInput from "./components/AddBudgetInput";
+import AddNewExpense from "./components/AddNewExpense";
 import DisplayAmountDetails from "./components/DisplayAmountDetails";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
   const [budget, setBudget] = useState(0);
   const [expense, setExpense] = useState(0);
   const [balance, setBalance] = useState(0);
+  const [allExpenses, setAllExpenses] = useState([]);
 
   const addBudget = (val) => {
     const result = budget + val;
@@ -23,21 +25,42 @@ export default function Home() {
     setBalance((prev) => parseInt(prev) + parseInt(val));
   };
 
+  const addNewExpense = (item) => {
+    // console.log(item);
+    let { amount } = item;
+    setBalance((prev) => parseInt(prev) - parseInt(amount));
+    setExpense((prev) => parseInt(prev) + parseInt(amount));
+    setAllExpenses((prev) => [...prev, item]);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.button}></View>
       <Button title="Add Budget" onPress={() => setVisibleAddBudget(true)} />
-      <AddBudgetInput
-        visibleAddBudget={visibleAddBudget}
-        setVisibleAddBudget={setVisibleAddBudget}
-        addBudget={addBudget}
-        budget={budget}
-      />
+      {!visibleAddExpense && (
+        <AddBudgetInput
+          visibleAddBudget={visibleAddBudget}
+          setVisibleAddBudget={setVisibleAddBudget}
+          addBudget={addBudget}
+          budget={budget}
+        />
+      )}
       <DisplayAmountDetails
         budget={budget}
         expense={expense}
         balance={balance}
       />
+      <Button
+        title="Add New Expense"
+        onPress={() => setVisibleAddExpense(true)}
+      />
+      {!visibleAddBudget && (
+        <AddNewExpense
+          setVisibleAddExpense={setVisibleAddExpense}
+          visibleAddExpense={visibleAddExpense}
+          addNewExpense={addNewExpense}
+        />
+      )}
     </View>
   );
 }
---
+++ src/components/AddNewExpense.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  Keyboard,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+export default function AddNewExpense({
+  visibleAddExpense,
+  setVisibleAddExpense,
+  addNewExpense,
+}) {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const closeModal = () => {
+    setTitle("");
+    setAmount("");
+    setVisibleAddExpense(false);
+  };
+
+  const handleSubmit = () => {
+    if (title !== "" && amount !== "" && amount != 0) {
+      Keyboard.dismiss();
+      closeModal();
+      addNewExpense({ title, amount: parseInt(amount) });
+    } else {
+      Alert.alert("Invalid", "Invalid input", [{ text: "OK" }]);
+    }
+  };
+
+  return (
+    <Modal visible={visibleAddExpense} animationType="slide">
+      <Text style={styles.title}>Add New Expense</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(val) => setTitle(val)}
+        placeholder="Enter Title"
+      />
+      <TextInput
+        style={styles.input}
+        value={amount}
+        onChangeText={(val) => setAmount(val)}
+        keyboardType="numeric"
+        placeholder="Enter Amount"
+      />
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title="cancel" color="darkgray" onPress={closeModal} />
+        </View>
+        <View style={styles.button}>
+          <Button title="add" color="green" onPress={handleSubmit} />
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    paddingVertical: 10,
+    marginBottom: 5,
+    borderBottomColor: "lightgray",
+    borderBottomWidth: 1,
+  },
+  input: {
+    margin: 10,
+    padding: 10,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    fontSize: 18,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    flex: 1,
+    margin: 10,
+  },
+});
